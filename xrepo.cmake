bb4ed5afab8580f8064588086f0ca9ef1414e3bf
Zhen <yingmanwumen@foxmail.com>
2025-09-28 20:08:14 +0800

    build: Update xmake and streamline Windows CI
    Update the default xmake version to 3.0.3.
    Remove redundant Visual Studio 2022 v144 toolset installation
    from Windows CI workflow, improving build efficiency. The toolset
    is now pre-installed on GitHub Actions runners.

--- xrepo.cmake
+++ xrepo.cmake
@@ -74,7 +74,7 @@ set(XREPO_XMAKEFILE "" CACHE STRING "Xmake script file of Xrepo package")
 
 function(_install_xmake_program)
     if (NOT XMAKE_RELEASE_LATEST)
-        set(XMAKE_RELEASE_LATEST 2.9.4)
+        set(XMAKE_RELEASE_LATEST 3.0.3)
     endif()
     set(XMAKE_VERSION master)
     set(XMAKE_BINARY_DIR ${CMAKE_BINARY_DIR}/xmake)
